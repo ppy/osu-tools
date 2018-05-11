8478be8ff8d25b665cc6e9ee1f078a31b40f5d9b
smoogipoo <smoogipoo@smgi.me>
2018-05-11 23:06:47 +0900

    Implement the performance processor

---
+++ PerformanceCalculator/PerformanceCalculator/Calculator.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-tools/master/LICENCE
+
+using System.Collections.Generic;
+using McMaster.Extensions.CommandLineUtils;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Platform;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets;
+using osu.Game.Rulesets.Scoring;
+
+namespace PerformanceCalculator
+{
+    public class Calculator : Component
+    {
+        private readonly string beatmapFile;
+        private readonly string[] replayFiles;
+        private readonly IConsole console;
+
+        public Calculator(string beatmapFile, string[] replayFiles, IConsole console)
+        {
+            this.beatmapFile = beatmapFile;
+            this.replayFiles = replayFiles;
+            this.console = console;
+        }
+
+        private WorkingBeatmap workingBeatmap;
+        private Ruleset ruleset;
+
+        [BackgroundDependencyLoader]
+        private void load(BeatmapManager beatmaps, ScoreStore scores, GameHost host)
+        {
+            if (workingBeatmap == null)
+                beatmaps.Import(new SingleFileArchiveReader(beatmapFile));
+
+            foreach (var f in replayFiles)
+            {
+                var score = scores.ReadReplayFile(f);
+
+                if (ruleset == null)
+                    ruleset = score.Ruleset.CreateInstance();
+
+                // Create beatmap
+                if (workingBeatmap == null)
+                    workingBeatmap = beatmaps.GetWorkingBeatmap(score.Beatmap);
+                workingBeatmap.Mods.Value = score.Mods;
+
+                // Convert + process beatmap
+                IBeatmap converted = ruleset.CreateBeatmapConverter(workingBeatmap.GetPlayableBeatmap(score.Ruleset)).Convert();
+                ruleset.CreateBeatmapProcessor(converted).PostProcess();
+
+                var categoryAttribs = new Dictionary<string, double>();
+                double pp = ruleset.CreatePerformanceCalculator(converted, score).Calculate(categoryAttribs);
+                
+                console.Out.WriteLine(f);
+                foreach (var kvp in categoryAttribs)
+                    console.Out.WriteLine($"{kvp.Key.PadRight(15)}: {kvp.Value}");
+                console.Out.WriteLine($"{"pp".PadRight(15)}: {pp}");
+            }
+            
+            host.Exit();
+        }
+    }
+}
---
+++ PerformanceCalculator/PerformanceCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-tools/master/LICENCE
+
+using System.ComponentModel.DataAnnotations;
+using JetBrains.Annotations;
+using McMaster.Extensions.CommandLineUtils;
+using osu.Framework.Platform;
+using osu.Game;
+
+namespace PerformanceCalculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CommandLineApplication.Execute<Program>(args);
+
+        [UsedImplicitly]
+        [Required, FileExists]
+        [Option(Template = "-b|--beatmap", Description = "Required. The beatmap for the replays.")]
+        public string Beatmap { get; }
+
+        [UsedImplicitly]
+        [Required, FileExists]
+        [Option(Template = "-r|--replay", Description = "One or more replays to calculate the performance for.")]
+        public string[] Replays { get; }
+
+        [UsedImplicitly]
+        public void OnExecute(CommandLineApplication app, IConsole console)
+        {
+            using (var host = new HeadlessGameHost("performance"))
+            {
+                var game = new OsuGameBase();
+                game.OnLoadComplete += _ => game.Add(new Calculator(Beatmap, Replays, console));
+
+                host.Run(game);
+            }
+        }
+    }
+}
