c409bbd0437e3b7aeafed33586e36df6d08093b0
smoogipoo <smoogipoo@smgi.me>
2018-05-12 13:13:35 +0900

    Fix license headers

--- PerformanceCalculator/PerformanceCalculator/Processor.cs
+++ PerformanceCalculator/PerformanceCalculator/Processor.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-tools/master/LICENCE
 
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
