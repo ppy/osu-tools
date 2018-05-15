fec61c5cf9664263e6820412dd9819bdec15cecb
smoogipoo <smoogipoo@smgi.me>
2018-05-15 13:13:22 +0900

    Give ProcessorWorkingBeatmap a valid ruleset

--- PerformanceCalculator/PerformanceCalculator/ProcessorWorkingBeatmap.cs
+++ PerformanceCalculator/PerformanceCalculator/ProcessorWorkingBeatmap.cs
@@ -6,6 +6,10 @@ using osu.Framework.Audio.Track;
 using osu.Framework.Graphics.Textures;
 using osu.Game.Beatmaps;
 using osu.Game.Beatmaps.Formats;
+using osu.Game.Rulesets.Catch;
+using osu.Game.Rulesets.Mania;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Taiko;
 
 namespace PerformanceCalculator
 {
@@ -40,6 +44,22 @@ namespace PerformanceCalculator
             : base(beatmap.BeatmapInfo)
         {
             this.beatmap = beatmap;
+
+            switch (beatmap.BeatmapInfo.RulesetID)
+            {
+                case 0:
+                    beatmap.BeatmapInfo.Ruleset = new OsuRuleset().RulesetInfo;
+                    break;
+                case 1:
+                    beatmap.BeatmapInfo.Ruleset = new TaikoRuleset().RulesetInfo;
+                    break;
+                case 2:
+                    beatmap.BeatmapInfo.Ruleset = new CatchRuleset().RulesetInfo;
+                    break;
+                case 3:
+                    beatmap.BeatmapInfo.Ruleset = new ManiaRuleset().RulesetInfo;
+                    break;
+            }
         }
 
         protected override IBeatmap GetBeatmap() => beatmap;
