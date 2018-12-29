1c20291b676a4fedf7c0dc5739ea23b8861001e9
Rohit Agarwal <agarrohit2020@gmail.com>
2018-12-29 14:53:38 -0500

    Cleanup

--- PerformanceCalculator/Profile/ProfileProcessor.cs
+++ PerformanceCalculator/Profile/ProfileProcessor.cs
@@ -4,21 +4,16 @@
 using System;
 using System.IO;
 using System.Net;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using McMaster.Extensions.CommandLineUtils;
-using osu.Game.Beatmaps;
-using osu.Game.Rulesets;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu;
 using osu.Game.Rulesets.Osu.Mods;
-using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.Scoring;
 using osu.Game.Scoring;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace PerformanceCalculator.Profile
 {
@@ -38,10 +33,9 @@ namespace PerformanceCalculator.Profile
             //get data for all 100 top plays
             var getPlayData = (HttpWebRequest) WebRequest.Create("https://osu.ppy.sh/api/get_user_best?k="+command.Key+"&u="+command.ProfileName+"&limit=100&type=username");
             HttpWebResponse response = (HttpWebResponse)getPlayData.GetResponse();
-            string json = null;
             var receiveStream = response.GetResponseStream();
             var readStream = new StreamReader(receiveStream);
-            json = readStream.ReadToEnd();
+            string json = readStream.ReadToEnd();
             var playData = JsonConvert.DeserializeObject<dynamic>(json);
             response.Close();
             receiveStream.Close();
@@ -90,17 +84,16 @@ namespace PerformanceCalculator.Profile
 
                 //Stats Calculation
                 var ruleset = new OsuRuleset();
-                var beatmap = workingBeatmap.GetPlayableBeatmap(ruleset.RulesetInfo);
 
                 int countmiss = (int)playData[i].countmiss;
                 int count50 = (int)playData[i].count50;
                 int count100 = (int)playData[i].count100;
                 int count300 = (int)playData[i].count300;
 
-                double accuracy = (double)(count50 + 2 * count100 + 6 * count300)/(6 * (countmiss + count50 + count100 + count300));
+                double accuracy = (double)(count50 + (2 * count100) + (6 * count300)) / (double)(6 * (countmiss + count50 + count100 + count300));
                 var maxCombo = (int)playData[i].maxcombo;
 
-                var statistics = new Dictionary<HitResult, object>()
+                var statistics = new Dictionary<HitResult, object>
                 {
                     {HitResult.Great, count300},
                     {HitResult.Good, count100},
@@ -110,36 +103,36 @@ namespace PerformanceCalculator.Profile
 
                 //mods are in a bitwise binary sum, with each mod given by the enum Mods
                 List<Mod> mods = new List<Mod>();
-                int enabled_mods = (int)playData[i].enabled_mods;
+                int enabledMods = (int)playData[i].enabled_mods;
 
-                if(((int)Mods.Hidden & enabled_mods) != 0)
+                if(((int)Mods.Hidden & enabledMods) != 0)
                     mods.Add(new OsuModHidden());
 
-                if(((int)Mods.Flashlight & enabled_mods) != 0)
+                if(((int)Mods.Flashlight & enabledMods) != 0)
                     mods.Add(new OsuModFlashlight());
 
                 //no need to include Nightcore because all nightcore maps also return DoubleTime by default
-                if(((int)Mods.DoubleTime & enabled_mods) != 0)
+                if(((int)Mods.DoubleTime & enabledMods) != 0)
                     mods.Add(new OsuModDoubleTime());
 
-                if(((int)Mods.HalfTime & enabled_mods) != 0)
+                if(((int)Mods.HalfTime & enabledMods) != 0)
                     mods.Add(new OsuModHalfTime());
 
-                if(((int)Mods.HardRock & enabled_mods) != 0)
+                if(((int)Mods.HardRock & enabledMods) != 0)
                     mods.Add(new OsuModHardRock());
 
-                if(((int)Mods.Easy & enabled_mods) != 0)
+                if(((int)Mods.Easy & enabledMods) != 0)
                     mods.Add(new OsuModEasy());
 
-                if(((int)Mods.NoFail & enabled_mods) != 0)
+                if(((int)Mods.NoFail & enabledMods) != 0)
                     mods.Add(new OsuModNoFail());
 
-                if(((int)Mods.SpunOut & enabled_mods) != 0)
+                if(((int)Mods.SpunOut & enabledMods) != 0)
                     mods.Add(new OsuModSpunOut());
 
                 Mod[] finalMods = mods.ToArray();
 
-                var scoreInfo = new ScoreInfo()
+                var scoreInfo = new ScoreInfo
                 {
                     Accuracy = accuracy,
                     MaxCombo = maxCombo,
@@ -151,9 +144,10 @@ namespace PerformanceCalculator.Profile
 
                 var categoryAttribs = new Dictionary<string, double>();
                 double pp = ruleset.CreatePerformanceCalculator(workingBeatmap, scoreInfo).Calculate(categoryAttribs);
-                var outputInfo = new PPInfo(){
-                    beatmapInfo = workingBeatmap.BeatmapInfo.ToString(),
-                    modInfo = finalMods.Length > 0
+                var outputInfo = new PPInfo
+                {
+                    BeatmapInfo = workingBeatmap.BeatmapInfo.ToString(),
+                    ModInfo = finalMods.Length > 0
                     ? finalMods.Select(m => m.Acronym).Aggregate((c, n) => $"{c}, {n}")
                     : "None"
                 };
@@ -163,16 +157,16 @@ namespace PerformanceCalculator.Profile
             int w = 0;
             foreach(KeyValuePair<double,PPInfo> kvp in sortedPP.Reverse())
             {
-                ppNet += Math.Pow(0.95,w)*(kvp.Key);
+                ppNet += Math.Pow(0.95,w)*kvp.Key;
 
-                writeAttribute((w+1) + ".Beatmap", kvp.Value.beatmapInfo);
-                writeAttribute("Mods", kvp.Value.modInfo);
-                writeAttribute("raw pp/weighted pp", kvp.Key.ToString() + " / " + (Math.Pow(0.95,w)*kvp.Key).ToString());
+                writeAttribute(w+1 + ".Beatmap", kvp.Value.BeatmapInfo);
+                writeAttribute("Mods", kvp.Value.ModInfo);
+                writeAttribute("raw pp/weighted pp", kvp.Key.ToString(CultureInfo.InvariantCulture) + " / " + (Math.Pow(0.95,w)*kvp.Key).ToString(CultureInfo.InvariantCulture));
                 w++;
             }
             //add on bonus pp
             ppNet += bonusPP;
-            writeAttribute("Top 100 Listed Above. Net PP", ppNet.ToString());
+            writeAttribute("Top 100 Listed Above. Net PP", ppNet.ToString(CultureInfo.InvariantCulture));
         }
 
         private void writeAttribute(string name, string value) => command.Console.WriteLine($"{name.PadRight(15)}: {value}");
