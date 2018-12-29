6f602719b3d51d5bb5b63b9fcb40e192a5803005
girantinas <44535964+girantinas@users.noreply.github.com>
2018-12-29 10:46:32 -0500

    Create ProfileCommand.cs

---
+++ PerformanceCalculator/Profile/ProfileCommand.cs
@@ -0,0 +1,30 @@
+// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-tools/master/LICENCE
+
+using System.ComponentModel.DataAnnotations;
+using JetBrains.Annotations;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace PerformanceCalculator.Profile
+{
+    [Command(Name = "profile", Description = "Returns the total pp of a profile.")]
+    public class ProfileCommand : ProcessorCommand
+    {
+        [UsedImplicitly]
+        [Required]
+        [Argument(0, Name = "profile name", Description = "Required. Name of the osu account (not user id)")]
+        public string ProfileName { get; }
+
+        [UsedImplicitly]
+        [Required]
+        [Argument(1, Name = "api key", Description = "Required. API Key, which you can get from here: https://osu.ppy.sh/p/api")]
+        public string Key { get; }
+
+        [UsedImplicitly]
+        [Required]
+        [Argument(2, Name = "path", Description = "Required. Path to a txt file that will be created. Must end with .txt")]
+        public string Path { get; }
+
+        protected override IProcessor CreateProcessor() => new ProfileProcessor(this);
+    }
+}
