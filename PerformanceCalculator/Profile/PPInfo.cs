2f07abf420f7fd3477d540f3762c86afd683603e
Rohit Agarwal <agarrohit2020@gmail.com>
2018-12-29 14:22:12 -0500

    Create PPInfo.cs

---
+++ PerformanceCalculator/Profile/PPInfo.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-tools/master/LICENCE
+
+namespace PerformanceCalculator.Profile
+{
+    public class PPInfo
+    {
+        public string beatmapInfo {get; set; }
+        public string modInfo {get; set; }
+    }
+}
