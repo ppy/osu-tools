41b7f8a10296344df3476f832b67796015294ba2
Kyle Chang <kyle.r.chang@gmail.com>
2018-12-29 21:46:18 -0500

    Fix license headers

--- PerformanceCalculator/Simulate/SimulateCommand.cs
+++ PerformanceCalculator/Simulate/SimulateCommand.cs
@@ -1,5 +1,5 @@
 // Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-tools/master/LICENCE
 
 using McMaster.Extensions.CommandLineUtils;
 using PerformanceCalculator.Simulate.Mania;
