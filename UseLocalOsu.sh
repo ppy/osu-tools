2947728e73072e5da32ad6eae2f9747568f8aa5c
smoogipoo <smoogipoo@smgi.me>
2021-03-22 18:09:48 +0900

    Use forward slashes in shell script

--- UseLocalOsu.sh
+++ UseLocalOsu.sh
@@ -7,11 +7,11 @@
 CSPROJ="PerformanceCalculator/PerformanceCalculator.csproj"
 SLN="osu.Tools.sln"
 
-DEPENDENCIES="..\osu\osu.Game.Rulesets.Catch\osu.Game.Rulesets.Catch.csproj
-    ..\osu\osu.Game.Rulesets.Mania\osu.Game.Rulesets.Mania.csproj
-    ..\osu\osu.Game.Rulesets.Osu\osu.Game.Rulesets.Osu.csproj
-    ..\osu\osu.Game.Rulesets.Taiko\osu.Game.Rulesets.Taiko.csproj
-    ..\osu\osu.Game\osu.Game.csproj"
+DEPENDENCIES="../osu/osu.Game.Rulesets.Catch/osu.Game.Rulesets.Catch.csproj
+    ../osu/osu.Game.Rulesets.Mania/osu.Game.Rulesets.Mania.csproj
+    ../osu/osu.Game.Rulesets.Osu/osu.Game.Rulesets.Osu.csproj
+    ../osu/osu.Game.Rulesets.Taiko/osu.Game.Rulesets.Taiko.csproj
+    ../osu/osu.Game/osu.Game.csproj"
 
 
 dotnet remove $CSPROJ package ppy.osu.Game
