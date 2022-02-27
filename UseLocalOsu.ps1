2ab53c4c455e0f671c258bc95cafa600f4e50d9b
StanR <hi@stanr.info>
2022-02-27 10:05:26 +0300

    Update UseLocalOsu scripts

--- UseLocalOsu.ps1
+++ UseLocalOsu.ps1
@@ -1,24 +1,30 @@
 # Run this script to use a local copy of osu rather than fetching it from nuget. 
 # It expects the osu directory to be at the same level as the osu-tools directory
 
+$PROJECTS=@(
+    "PerformanceCalculator/PerformanceCalculator.csproj"
+    "PerformanceCalculatorGUI/PerformanceCalculatorGUI.csproj"
+)
 
-$CSPROJ="PerformanceCalculator/PerformanceCalculator.csproj"
 $SLN="osu.Tools.sln"
 
 $DEPENDENCIES=@(
-    "..\osu\osu.Game.Rulesets.Catch\osu.Game.Rulesets.Catch.csproj"
-    "..\osu\osu.Game.Rulesets.Mania\osu.Game.Rulesets.Mania.csproj"
-    "..\osu\osu.Game.Rulesets.Osu\osu.Game.Rulesets.Osu.csproj"
-    "..\osu\osu.Game.Rulesets.Taiko\osu.Game.Rulesets.Taiko.csproj"
-    "..\osu\osu.Game\osu.Game.csproj"
+    ".\osu\osu.Game.Rulesets.Catch\osu.Game.Rulesets.Catch.csproj"
+    ".\osu\osu.Game.Rulesets.Mania\osu.Game.Rulesets.Mania.csproj"
+    ".\osu\osu.Game.Rulesets.Osu\osu.Game.Rulesets.Osu.csproj"
+    ".\osu\osu.Game.Rulesets.Taiko\osu.Game.Rulesets.Taiko.csproj"
+    ".\osu\osu.Game\osu.Game.csproj"
 )
 
-
-dotnet remove $CSPROJ package ppy.osu.Game
-dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Osu
-dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Taiko
-dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Catch
-dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Mania
-
 dotnet sln $SLN add $DEPENDENCIES
-dotnet add $CSPROJ reference $DEPENDENCIES
+
+ForEach ($CSPROJ in $PROJECTS)
+{
+    dotnet remove $CSPROJ package ppy.osu.Game
+    dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Osu
+    dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Taiko
+    dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Catch
+    dotnet remove $CSPROJ package ppy.osu.Game.Rulesets.Mania
+
+    dotnet add $CSPROJ reference $DEPENDENCIES
+}
