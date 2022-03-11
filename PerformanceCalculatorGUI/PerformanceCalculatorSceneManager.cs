e511b811a13e76bdaf647d51c70f45c2a577ece4
StanR <hi@stanr.info>
2022-03-11 17:04:19 +0300

    Add missing license headers

--- PerformanceCalculatorGUI/PerformanceCalculatorSceneManager.cs
+++ PerformanceCalculatorGUI/PerformanceCalculatorSceneManager.cs
@@ -135,7 +135,7 @@ namespace PerformanceCalculatorGUI
                                     }
                                 }
                             }
-                        } 
+                        }
                     }
                 }
             };
