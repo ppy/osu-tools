b1d50068705252483f7b32f5d548924922dce683
Dean Herbert <pe@ppy.sh>
2022-07-24 23:35:03 +0900

    Fix more incorectly named classes / files

---
+++ PerformanceCalculatorGUI/ExtendedOsuDifficultyCalculator.cs
@@ -0,0 +1,32 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Linq;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets;
+using osu.Game.Rulesets.Difficulty;
+using osu.Game.Rulesets.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Difficulty.Skills;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Osu.Difficulty;
+
+namespace PerformanceCalculatorGUI;
+
+public class ExtendedOsuDifficultyCalculator : OsuDifficultyCalculator, IExtendedDifficultyCalculator
+{
+    private Skill[] skills;
+
+    public ExtendedOsuDifficultyCalculator(IRulesetInfo ruleset, IWorkingBeatmap beatmap)
+        : base(ruleset, beatmap)
+    {
+    }
+
+    public Skill[] GetSkills() => skills;
+    public DifficultyHitObject[] GetDifficultyHitObjects(IBeatmap beatmap, double clockRate) => CreateDifficultyHitObjects(beatmap, clockRate).ToArray();
+
+    protected override DifficultyAttributes CreateDifficultyAttributes(IBeatmap beatmap, Mod[] mods, Skill[] skills, double clockRate)
+    {
+        this.skills = skills;
+        return base.CreateDifficultyAttributes(beatmap, mods, skills, clockRate);
+    }
+}
