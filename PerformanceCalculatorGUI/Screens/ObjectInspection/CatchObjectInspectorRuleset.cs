c67393ddefc1d1d6953afb550bcdd32b421a09a1
StanR <hi@stanr.info>
2024-06-25 23:19:35 +0500

    Fix object inspectors not working

--- PerformanceCalculatorGUI/Screens/ObjectInspection/CatchObjectInspectorRuleset.cs
+++ PerformanceCalculatorGUI/Screens/ObjectInspection/CatchObjectInspectorRuleset.cs
@@ -4,21 +4,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Game.Beatmaps;
-using osu.Game.Graphics;
 using osu.Game.Rulesets;
 using osu.Game.Rulesets.Catch.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Catch.Edit;
-using osu.Game.Rulesets.Catch.UI;
 using osu.Game.Rulesets.Mods;
-using osu.Game.Rulesets.UI;
 
 namespace PerformanceCalculatorGUI.Screens.ObjectInspection
 {
-    public partial class CatchObjectInspectorRuleset : DrawableCatchRuleset
+    public partial class CatchObjectInspectorRuleset : DrawableCatchEditorRuleset
     {
         private readonly CatchDifficultyHitObject[] difficultyHitObjects;
 
@@ -36,36 +30,12 @@ namespace PerformanceCalculatorGUI.Screens.ObjectInspection
 
         public override bool PropagateNonPositionalInputSubTree => false;
 
-        protected override Playfield CreatePlayfield() => new CatchObjectInspectorPlayfield(Beatmap.Difficulty);
+        public override bool AllowBackwardsSeeks => true;
 
         protected override void Update()
         {
             base.Update();
             objectDifficultyValuesContainer.CurrentDifficultyHitObject.Value = difficultyHitObjects.LastOrDefault(x => x.StartTime < Clock.CurrentTime);
-        }
-
-        private partial class CatchObjectInspectorPlayfield : CatchEditorPlayfield
-        {
-            protected override GameplayCursorContainer CreateCursor() => null!;
-
-            public CatchObjectInspectorPlayfield(IBeatmapDifficultyInfo difficulty)
-                : base(difficulty)
-            {
-                DisplayJudgements.Value = false;
-                AddInternal(new Container
-                {
-                    RelativeSizeAxes = Axes.X,
-                    Y = 440,
-                    Height = 6.0f,
-                    CornerRadius = 4.0f,
-                    Masking = true,
-                    Child = new Box
-                    {
-                        Colour = OsuColour.Gray(0.5f),
-                        RelativeSizeAxes = Axes.Both
-                    }
-                });
-            }
         }
     }
 }
