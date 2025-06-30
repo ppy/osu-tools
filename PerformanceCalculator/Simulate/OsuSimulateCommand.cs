a20964a9fad4027680a16797a3f03500561d44fa
Givikap120 <givikap120@gmail.com>
2025-06-30 23:53:02 +0300

    do the same for CLI

--- PerformanceCalculator/Simulate/OsuSimulateCommand.cs
+++ PerformanceCalculator/Simulate/OsuSimulateCommand.cs
@@ -50,19 +50,31 @@ namespace PerformanceCalculator.Simulate
             // Use lazer info only if score has sliderhead accuracy
             if (mods.OfType<OsuModClassic>().Any(m => m.NoSliderHeadAccuracy.Value))
             {
-                return generateHitResults(beatmap, Accuracy / 100, Misses, Mehs, Goods, null, null);
+                return generateHitResults(beatmap, Accuracy / 100, mods, Misses, Mehs, Goods, null, null);
             }
             else
             {
-                return generateHitResults(beatmap, Accuracy / 100, Misses, Mehs, Goods, largeTickMisses, sliderTailMisses);
+                return generateHitResults(beatmap, Accuracy / 100, mods, Misses, Mehs, Goods, largeTickMisses, sliderTailMisses);
             }
         }
 
-        private static Dictionary<HitResult, int> generateHitResults(IBeatmap beatmap, double accuracy, int countMiss, int? countMeh, int? countGood, int? countLargeTickMisses, int? countSliderTailMisses)
+        private static Dictionary<HitResult, int> generateHitResults(IBeatmap beatmap, double accuracy, Mod[] mods, int countMiss, int? countMeh, int? countGood, int? countLargeTickMisses, int? countSliderTailMisses)
         {
+            bool usingClassicSliderAccuracy = mods.OfType<OsuModClassic>().Any(m => m.NoSliderHeadAccuracy.Value);
+
             int countGreat;
 
             int totalResultCount = beatmap.HitObjects.Count;
+
+            int countLargeTicks = beatmap.HitObjects.Sum(obj => obj.NestedHitObjects.Count(x => x is SliderTick or SliderRepeat));
+            int countSmallTicks = beatmap.HitObjects.Count(x => x is Slider);
+
+            // Sliderheads are large ticks too if slideracc is disabled
+            if (usingClassicSliderAccuracy)
+                countLargeTicks += countSmallTicks;
+
+            countLargeTickMisses = Math.Min(countLargeTickMisses ?? 0, countLargeTicks);
+            countSliderTailMisses = Math.Min(countSliderTailMisses ?? 0, countSmallTicks);
 
             if (countMeh != null || countGood != null)
             {
@@ -70,8 +82,25 @@ namespace PerformanceCalculator.Simulate
             }
             else
             {
-                // Total result count excluding countMiss
-                int relevantResultCount = totalResultCount - countMiss;
+                // If there's no classic slider accuracy - we need to weight circle judgements accordingly
+                double normalJudgementWeight = 1.0;
+
+                // Relevant result count without misses (normal misses and slider-related misses
+                // We need to exclude them from judgement count so total value will be equal to desired after misses are accounted for
+                double relevantResultCount;
+
+                if (usingClassicSliderAccuracy)
+                {
+                    relevantResultCount = totalResultCount - countMiss;
+                }
+                else
+                {
+                    double maxSliderPortion = countSmallTicks * 0.5 + countLargeTicks * 0.1;
+                    normalJudgementWeight = (totalResultCount + maxSliderPortion) / totalResultCount;
+
+                    double missedSliderPortion = (double)countSliderTailMisses * 0.5 + (double)countLargeTickMisses * 0.1;
+                    relevantResultCount = totalResultCount - (countMiss + missedSliderPortion) / normalJudgementWeight;
+                }
 
                 // Accuracy excluding countMiss. We need that because we're trying to achieve target accuracy without touching countMiss
                 // So it's better to pretened that there were 0 misses in the 1st place
@@ -87,7 +116,7 @@ namespace PerformanceCalculator.Simulate
                     double ratio50To100 = Math.Pow(1 - (relevantAccuracy - 0.25) / 0.75, 2);
 
                     // Derived from the formula: Accuracy = (6 * c300 + 2 * c100 + c50) / (6 * totalHits), assuming that c50 = c100 * ratio50to100
-                    double count100Estimate = 6 * relevantResultCount * (1 - relevantAccuracy) / (5 * ratio50To100 + 4);
+                    double count100Estimate = 6 * relevantResultCount * (1 - relevantAccuracy) / (5 * ratio50To100 + 4) * normalJudgementWeight;
 
                     // Get count50 according to c50 = c100 * ratio50to100
                     double count50Estimate = count100Estimate * ratio50To100;
@@ -108,17 +137,17 @@ namespace PerformanceCalculator.Simulate
                     double count50Estimate = relevantResultCount - count100Estimate;
 
                     // Round it to get int number of 100s
-                    countGood = (int?)Math.Round(count100Estimate);
+                    countGood = (int?)Math.Round(count100Estimate * normalJudgementWeight);
 
                     // Get number of 50s as difference between total mistimed hits and count100
-                    countMeh = (int?)(Math.Round(count100Estimate + count50Estimate) - countGood);
+                    countMeh = (int?)(Math.Round((count100Estimate + count50Estimate) * normalJudgementWeight) - countGood);
                 }
                 // If accuracy is less than 16.67% - it means that we have only 50s or misses
                 // Assuming that we removed misses in the 1st place - that means that we need to add additional misses to achieve target accuracy
                 else
                 {
                     // Derived from the formula: Accuracy = (6 * c300 + 2 * c100 + c50) / (6 * totalHits), assuming that c300 = c100 = 0
-                    double count50Estimate = 6 * relevantResultCount * relevantAccuracy;
+                    double count50Estimate = 6 * (totalResultCount - countMiss) * relevantAccuracy;
 
                     // We have 0 100s, because we can't start adding 100s again after reaching "only 50s" point
                     countGood = 0;
@@ -129,6 +158,10 @@ namespace PerformanceCalculator.Simulate
                     // Fill the rest results with misses overwriting initial countMiss
                     countMiss = (int)(totalResultCount - countMeh);
                 }
+
+                // Clamp goods if total amount is bigger than possible
+                countGood -= Math.Clamp((int)(countGood + countMeh + countMiss - totalResultCount), 0, (int)countGood);
+                countMeh -= Math.Clamp((int)(countGood + countMeh + countMiss - totalResultCount), 0, (int)countMeh);
 
                 // Rest of the hits are 300s
                 countGreat = (int)(totalResultCount - countGood - countMeh - countMiss);
@@ -143,16 +176,24 @@ namespace PerformanceCalculator.Simulate
             };
 
             if (countLargeTickMisses != null)
-                result[HitResult.LargeTickMiss] = countLargeTickMisses.Value;
+            {
+                result[HitResult.LargeTickHit] = countLargeTicks - (int)countLargeTickMisses;
+                result[HitResult.LargeTickMiss] = (int)countLargeTickMisses;
+            }
 
             if (countSliderTailMisses != null)
-                result[HitResult.SliderTailHit] = beatmap.HitObjects.Count(x => x is Slider) - countSliderTailMisses.Value;
+            {
+                result[usingClassicSliderAccuracy ? HitResult.SmallTickHit : HitResult.SliderTailHit] = countSmallTicks - (int)countSliderTailMisses;
+                if (usingClassicSliderAccuracy) result[HitResult.SmallTickMiss] = (int)countSliderTailMisses;
+            }
 
             return result;
         }
 
         protected override double GetAccuracy(IBeatmap beatmap, Dictionary<HitResult, int> statistics, Mod[] mods)
         {
+            bool usingClassicSliderAccuracy = mods.OfType<OsuModClassic>().Any(m => m.NoSliderHeadAccuracy.Value);
+
             int countGreat = statistics[HitResult.Great];
             int countGood = statistics[HitResult.Ok];
             int countMeh = statistics[HitResult.Meh];
@@ -161,7 +202,7 @@ namespace PerformanceCalculator.Simulate
             double total = 6 * countGreat + 2 * countGood + countMeh;
             double max = 6 * (countGreat + countGood + countMeh + countMiss);
 
-            if (statistics.TryGetValue(HitResult.SliderTailHit, out int countSliderTailHit))
+            if (!usingClassicSliderAccuracy && statistics.TryGetValue(HitResult.SliderTailHit, out int countSliderTailHit))
             {
                 int countSliders = beatmap.HitObjects.Count(x => x is Slider);
 
@@ -169,10 +210,10 @@ namespace PerformanceCalculator.Simulate
                 max += 3 * countSliders;
             }
 
-            if (statistics.TryGetValue(HitResult.LargeTickMiss, out int countLargeTickMiss))
+            if (!usingClassicSliderAccuracy && statistics.TryGetValue(HitResult.LargeTickMiss, out int countLargeTicksMiss))
             {
                 int countLargeTicks = beatmap.HitObjects.Sum(obj => obj.NestedHitObjects.Count(x => x is SliderTick or SliderRepeat));
-                int countLargeTickHit = countLargeTicks - countLargeTickMiss;
+                int countLargeTickHit = countLargeTicks - countLargeTicksMiss;
 
                 total += 0.6 * countLargeTickHit;
                 max += 0.6 * countLargeTicks;
