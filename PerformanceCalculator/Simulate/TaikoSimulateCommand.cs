4707ec56aea9d573e416525b00e22bc705219ba0
minisbett <39670899+minisbett@users.noreply.github.com>
2026-09-10 20:51:10 +0200

    rename hitCount to relevantResultCount

--- PerformanceCalculator/Simulate/TaikoSimulateCommand.cs
+++ PerformanceCalculator/Simulate/TaikoSimulateCommand.cs
@@ -45,8 +45,8 @@ namespace PerformanceCalculator.Simulate
             else
             {
                 // Let Great=2, Good=1, Miss=0. The total should be this.
-                int hitCount = totalResultCount - countMiss;
-                int targetTotal = (int)Math.Round(accuracy * hitCount * 2);
+                int relevantResultCount = totalResultCount - countMiss;
+                int targetTotal = (int)Math.Round(accuracy * relevantResultCount * 2);
 
                 countGreat = targetTotal - (totalResultCount - countMiss);
                 countGood = totalResultCount - countGreat - countMiss;
